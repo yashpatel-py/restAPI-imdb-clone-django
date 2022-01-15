bdfaa577f5fd4738226aec0dec1145a769a77e98
Yash Patel <mailforyash04@gmail.com>
2022-01-15 21:46:35 +0530

    ✅ Model and migrations - DONE

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,3 +1,6 @@
+import imp
 from django.contrib import admin
+from .models import Movie
 
 # Register your models here.
+admin.site.register(Movie)
--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -1,3 +1,12 @@
+from statistics import mode
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
