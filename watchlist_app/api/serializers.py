9751927f77633f528da34d58f3a3c09a13cd0495
yash-2115 <mailforyash04@gmail.com>
2022-01-20 20:48:51 +0530

    🔥 Added validation

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,9 +1,14 @@
+from wsgiref.validate import validator
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+def name_length(value):
+    if len(value) < 2:
+        raise serializers.ValidationError("Name is too short!")
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only = True)
-    name = serializers.CharField()
+    # Validate a single field in serializer it self
+    name = serializers.CharField(validators = [name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
@@ -18,3 +23,16 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    # Validation for complete objects
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError("Title and Description should be different!")
+        else:
+            return data
+
+    # Calidation for single objects
+    # def validate_name(self, value):
+    #     if len(value) < 2:
+    #         raise serializers.ValidationError("Name is too short!")
+    #     return value
