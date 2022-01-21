1cd55e48eb4e81676884056ad506ada3b609bf44
yash-2115 <mailforyash04@gmail.com>
2022-01-21 22:30:44 +0530

    ⚡️

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -3,8 +3,6 @@ from watchlist_app.api import views
 
 
 urlpatterns = [
-    # path('list/', views.movie_list, name='movie-list'),
-    # path('<int:pk>', views.movie_detail, name="movie-detail")
     path('list/', views.WatchListAV.as_view(), name='movie-list'),
     path('<int:pk>', views.WatchDetailAV.as_view(), name="movie-detail"),
     path('platform/', views.StreamPlatformAV.as_view(), name='platform-list'),
