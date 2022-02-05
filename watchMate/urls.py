107123f45c7be343a4e34650b37a488a9fb93612
yash-2115 <mailforyash04@gmail.com>
2022-02-06 01:12:40 +0530

    added rest_framework urls for temp login form

--- watchMate/urls.py
+++ watchMate/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('watch/', include('watchlist_app.api.urls')),
+    path('api-auth', include('rest_framework.urls')),
 ]
