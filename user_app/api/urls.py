466639c5ec48fa8f2356243d994239718d12b94e
Yash Patel <mailforyash04@gmail.com>
2022-02-16 00:09:54 +0530

    Logout and delete token

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -5,4 +5,5 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('login/', obtain_auth_token, name="login"),
     path('register/', views.registration_view, name="register"),
+    path('logout/', views.logOut_view, name="logout"),
 ]
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -3,7 +3,13 @@ from .serializers import ResgistrationSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from user_app import models
+from rest_framework import status
 
+@api_view(['POST',])
+def logOut_view(request):
+    if request.method == "POST":
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
 
 @api_view(['POST',])
 def registration_view(request):
