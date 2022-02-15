86f6a132b914f9a3b3df0ad6edc0a103e5317dfd
Yash Patel <mailforyash04@gmail.com>
2022-02-15 23:53:30 +0530

    Auto generate token while registration

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,12 +1,26 @@
 from rest_framework.decorators import api_view
 from .serializers import ResgistrationSerializer
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from user_app import models
 
 
 @api_view(['POST',])
 def registration_view(request):
     if request.method == "POST":
         serializer = ResgistrationSerializer(data=request.data)
+
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+            account = serializer.save()
+
+            data['response'] = "Registration successful!!!"
+            data['username'] = account.username
+            data['email'] = account.email
+
+            token = Token.objects.get(user=account).key
+            data['token'] = token 
+        else:
+            data = serializer.errors
+        return Response(data)
