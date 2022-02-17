ea6f3e263318e0df0b4889a2a32f658dac394e3d
Yash Patel <mailforyash04@gmail.com>
2022-02-17 09:50:41 +0530

    JWT Setup

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('login/', obtain_auth_token, name="login"),
     path('register/', views.registration_view, name="register"),
     path('logout/', views.logOut_view, name="logout"),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- watchMate/settings.py
+++ watchMate/settings.py
@@ -127,6 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
