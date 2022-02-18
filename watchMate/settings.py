6f1eebc347f81b10cfa8a37d9f3e5b3c545fea4c
Yash Patel <mailforyash04@gmail.com>
2022-02-18 22:22:07 +0530

    global settings for throttling rate

--- watchMate/settings.py
+++ watchMate/settings.py
@@ -130,7 +130,15 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '3/day'
+    }
 }
 
 SIMPLE_JWT = {
