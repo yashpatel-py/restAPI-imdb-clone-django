1b0ddc71c586ce679145e3540731a67ad85f343d
Yash Patel <mailforyash04@gmail.com>
2022-05-25 21:17:50 +0530

    Some basic update

--- watchMate/urls.py
+++ watchMate/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('watch/', include('watchlist_app.api.urls')),
+    path('', include('watchlist_app.api.urls')),
     path('account/', include('user_app.api.urls')),
 ]
